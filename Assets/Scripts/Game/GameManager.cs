5ca3db13a5e8473ec98d191a402b0bf8bcdc8c6d
Rostozzza <gfiyskgs@gmail.com>
2024-11-17 02:10:04 +0300

    выбор юнита + начало добавления комплексов

--- Assets/Scripts/Game/GameManager.cs
+++ Assets/Scripts/Game/GameManager.cs
@@ -10,6 +10,8 @@ public class GameManager : MonoBehaviour
 	private int honey;
 	private int asteriy;
 
+	RaycastHit raycastHit;
+
 	public void Awake()
 	{
 		if (Instance == null)
@@ -20,7 +22,7 @@ public class GameManager : MonoBehaviour
 	}
 	
 	/// <summary>
-	/// Возвращает текущее значение мёда на клиенте.
+	/// пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅ пїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅ.
 	/// </summary>
 	/// <returns></returns>
 	public int GetHoney()
@@ -29,7 +31,7 @@ public class GameManager : MonoBehaviour
 	}
 	
 	/// <summary>
-	/// Возвращает текущее значение астерия на клиенте.
+	/// пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅ.
 	/// </summary>
 	/// <returns></returns>
 	public int GetAsteriy()
@@ -38,7 +40,7 @@ public class GameManager : MonoBehaviour
 	}
 
 	/// <summary>
-	/// Изменяет количество мёда.
+	/// пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅ.
 	/// </summary>
 	/// <param name="amount"></param>
 	public void ChangeHoney(int amount)
@@ -47,11 +49,34 @@ public class GameManager : MonoBehaviour
 	}
 
 	/// <summary>
-	/// Изменяет количество астерия.
+	/// пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅ.
 	/// </summary>
 	/// <param name="amount"></param>
 	public void ChangeAsteriy(int amount)
 	{
 		asteriy += amount;
+	}
+
+	private void Update()
+	{
+		if (Input.GetMouseButtonDown(0))
+		{
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out raycastHit, 100f))
+            {
+                if (raycastHit.transform != null)
+                {
+                    ClickedGameObject(raycastHit.transform.gameObject);
+                }
+            }
+		}
+	}
+
+	public void ClickedGameObject(GameObject gameObject)
+	{
+		if (gameObject.CompareTag("unit"))
+		{
+			gameObject.GetComponent<UnitScript>().ChooseUnit();
+		}
 	}
 }
