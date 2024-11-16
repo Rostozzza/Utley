9934fc6659613a60026e4aca56fa9a3368c77834
Rostozzza <gfiyskgs@gmail.com>
2024-11-16 18:40:24 +0300

    мувмент одного юнита, переключается булевой

---
+++ Assets/Scripts/UnitMove.cs
@@ -0,0 +1,59 @@
+using UnityEditor.Callbacks;
+using UnityEngine;
+
+public class UnitMove : MonoBehaviour
+{
+    [SerializeField] public bool isFollowing;
+    [SerializeField] public float speed;
+    private bool onLadder;
+    private Rigidbody rb;
+    private Vector3 dir;
+    private int laddersAmount;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        onLadder = laddersAmount > 0;
+
+        if (isFollowing)
+        {
+            rb.useGravity = !onLadder;
+            dir = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical") * (onLadder ? 1f : 0f), 0f);
+            rb.linearVelocity = new Vector3(speed * dir.x, speed * dir.y, 0f);
+        }
+    }
+
+    private bool CheckLadder()
+    {
+        bool check = false;
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(transform.position, 1f);
+        for (int i = 0; i < colliders.Length; i++)
+        {
+            if (colliders[i].CompareTag("ladder_room"))
+            {
+                check = true;
+            }
+        }
+        return check;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("ladder_room"))
+        {
+            laddersAmount++;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("ladder_room"))
+        {
+            laddersAmount--;
+        }
+    }
+}
