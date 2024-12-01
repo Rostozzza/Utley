7a540408bd9224e389d239e5f55b9b87a9d28d5d
Tim16763 <94013118+RazoV1@users.noreply.github.com>
2024-12-02 01:21:35 +0300

    Доделал меню настроек

---
+++ Assets/Scripts/UI/ResolutionSelecter.cs
@@ -0,0 +1,39 @@
+﻿
+using System.Collections.Generic;
+using System.Linq;
+using TMPro;
+using UnityEngine;
+
+public class ResolutionSelecter : MonoBehaviour
+{
+	[SerializeField] private TMP_Dropdown resolutionDropdown;
+	private List<Resolution> resolutions;
+	private float currentRefreshRate;
+	private int currentResolutionIndex = 0;
+
+	public void Start()
+	{
+		resolutions = Screen.resolutions.ToList();
+		resolutionDropdown.ClearOptions();
+		List<string> options = resolutions.Select(x => x.width + "x" + x.height).ToList();
+		
+		resolutionDropdown.AddOptions(options);
+		if (!PlayerPrefs.HasKey("SavedResolution"))
+		{
+			currentResolutionIndex = resolutions.IndexOf(Screen.currentResolution);
+		}
+		else
+		{
+			currentResolutionIndex = PlayerPrefs.GetInt("SavedResolution");
+		}
+		resolutionDropdown.value = currentResolutionIndex;
+		resolutionDropdown.RefreshShownValue();
+	}
+
+	public void SetResolution(int index)
+	{
+		Resolution resolution = resolutions[index];
+		Screen.SetResolution(resolution.width, resolution.height, true);
+		PlayerPrefs.SetInt("SavedResolution",index);
+	}
+}
