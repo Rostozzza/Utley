c1183f3482c8fab47c61dfe662e9297e7e10a0f5
Rostozzza <gfiyskgs@gmail.com>
2024-12-05 23:40:52 +0300

    Левый список + температура

--- Assets/Scripts/UI/RoomStatusController.cs
+++ Assets/Scripts/UI/RoomStatusController.cs
@@ -1,25 +1,42 @@
-using System;
-using TMPro;
 using UnityEngine;
+using TMPro;
+using System;
+using UnityEngine.UI;
+using System.Collections.Generic;
 
 public class RoomStatusController : MonoBehaviour
 {
+    [SerializeField] private Image panel;
     [SerializeField] private TextMeshProUGUI roomName;
     [SerializeField] private TextMeshProUGUI durabilityShow;
     [SerializeField] private TextMeshProUGUI stateShow;
     [SerializeField] private GameObject obj;
+    [SerializeField] private Image icon;
+    [SerializeField] private List<Sprite> icons;
+    private Color defaultColor;
 
-    public void Init(GameObject obj, string name, float durability, RoomScript.Status status)
+    private void Start()
+    {
+        defaultColor = panel.color;
+    }
+
+    public void Init(GameObject obj, string name, float durability, RoomScript.Status status, RoomScript.Resources resource)
     {
         this.obj = obj;
         roomName.text = name;
         durabilityShow.text = DurabilityToText(durability);
         stateShow.text = StatusToText(status);
+        icon.sprite = ResourceTypeToSprite(resource);
+    }
+
+    private Sprite ResourceTypeToSprite(RoomScript.Resources resource)
+    {
+        return icons[(int)resource];
     }
 
     private string DurabilityToText(float durability)
     {
-        return Convert.ToString((int)(durability * 100)) + "%";
+        return Convert.ToString((int)(durability * 100)) + "/100";
     }
 
     private string StatusToText(RoomScript.Status status)
@@ -27,7 +44,7 @@ public class RoomStatusController : MonoBehaviour
         switch (status)
         {
             case RoomScript.Status.Destroyed:
-                return "Разрушено";
+                return "Сломан";
             case RoomScript.Status.Busy:
                 return "Работает";
             case RoomScript.Status.Free:
@@ -44,6 +61,28 @@ public class RoomStatusController : MonoBehaviour
 
     public void UpdateStatus(RoomScript.Status status)
     {
+        switch (status)
+        {
+            case RoomScript.Status.Free:
+                panel.color = defaultColor;
+                stateShow.color = Color.white;
+                durabilityShow.color = Color.white;
+                icon.color = Color.white;
+                break;
+            case RoomScript.Status.Busy:
+                panel.color = defaultColor;
+                stateShow.color = (Color.red + Color.yellow) / 2f;
+                durabilityShow.color = Color.white;
+                icon.color = Color.white;
+                break;
+            case RoomScript.Status.Destroyed:
+                panel.color = Color.red;
+                stateShow.color = Color.black;
+                durabilityShow.color = Color.black;
+                icon.color = Color.black;
+
+                break;
+        }
         stateShow.text = StatusToText(status);
     }
 
