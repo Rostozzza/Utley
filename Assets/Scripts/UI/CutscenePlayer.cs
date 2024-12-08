de556ab749d2c529778860b3f79b6012aba0be3d
Rostozzza <gfiyskgs@gmail.com>
2024-12-08 12:57:45 +0300

    Почти катсцены

---
+++ Assets/Scripts/UI/CutscenePlayer.cs
@@ -0,0 +1,23 @@
+using System.Collections.Generic;
+using UnityEditor.SearchService;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Video;
+using System.Collections;
+
+public class CutscenePlayer : MonoBehaviour
+{
+    
+
+    //[SerializeField] List<VideoClip> videos;
+    //[SerializeField] private int clipNumber = 0;
+    
+    
+
+    private void Start()
+    {
+    }
+
+
+
+}
