66e7d56a8fa33aa5c8dacb124c946b30acb6fa49
Rostozzza <gfiyskgs@gmail.com>
2025-01-20 21:35:20 +0300

    Обводка (RoomOutliner), цвет настраивается прямо в коде

---
+++ Assets/Scripts/RoomScripts/RoomOutliner.cs
@@ -0,0 +1,32 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomOutliner : MonoBehaviour
+{
+    [SerializeField] private List<SpriteRenderer> outlineFrameParts;
+    private Color color = Color.blue;
+    void Start()
+    {
+        if (outlineFrameParts.Count == 0)
+        {
+            Debug.Log("Outline frames is missing in " + name + "!!!");
+        }
+        else // things to do at start if all right
+        {
+            outlineFrameParts.ForEach(x => x.color = color);
+            SetOutline(false);
+        }
+    }
+
+    /// <summary>
+    /// Shows or hides outline by bool (show - true / hide - false)
+    /// </summary>
+    /// <param name="set"></param>
+    public void SetOutline(bool set)
+    {
+        foreach (var framePart in outlineFrameParts)
+        {
+            framePart.enabled = set;
+        }
+    }
+}
