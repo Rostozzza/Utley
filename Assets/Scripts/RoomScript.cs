c606ba34215a6c9173662bf19faaf32aba4d0711
Rostozzza <gfiyskgs@gmail.com>
2025-03-18 19:15:30 +0300

    Подфиксил нехватку астерия для ремонта и т.п.

--- Assets/Scripts/RoomScript.cs
+++ Assets/Scripts/RoomScript.cs
@@ -371,7 +371,7 @@ public class RoomScript : MonoBehaviour
 		{
 			Debug.Log("Не хватает ресов для починки!");
 			//EventManager.callWarning.Invoke($"Не хватает <color=yellow>{Mathf.CeilToInt((30 + 10 * (level - 1)) - await GameManager.Instance.GetHoney())}</color> энергомеда для починки!");
-			EventManager.callWarning.Invoke(NotEnoughResources(requireAsterium - await GameManager.Instance.GetAsteriy(), requireAstroluminite - await GameManager.Instance.GetAstroluminite()));
+			EventManager.callWarning.Invoke(NotEnoughResources(requireAsterium - await GameManager.Instance.GetAsteriy(), requireAstroluminite - await GameManager.Instance.GetAstroluminite()) + " для улучшения!");
 			return;
 		}
 	}
@@ -383,7 +383,7 @@ public class RoomScript : MonoBehaviour
 		if (differenceAsterium > 0) toReturn += $"<color=yellow>{Mathf.CeilToInt(differenceAsterium)}</color> астерия, ";
 		if (differenceAstroluminite > 0) toReturn += $"<color=yellow>{Mathf.CeilToInt(differenceAstroluminite)}</color> астролюминита, ";
 
-		toReturn = toReturn[..^2] + " для починки!";
+		toReturn = toReturn[..^2];
 
 		return toReturn;
 	}
@@ -444,7 +444,7 @@ public class RoomScript : MonoBehaviour
 				await GameManager.Instance.GetAstroluminite() < requireAstroluminite)//(currentHoney < (30 + 10 * (level - 1)));
 			{
 				//desiredButton.GetComponent<Button>() = false;
-				desiredButton.text = NotEnoughResources(requireAsterium - await GameManager.Instance.GetAsteriy(), requireAstroluminite - await GameManager.Instance.GetAstroluminite());
+				desiredButton.text = NotEnoughResources(requireAsterium - await GameManager.Instance.GetAsteriy(), requireAstroluminite - await GameManager.Instance.GetAstroluminite()) + " для улучшения!";
 			}
 			else
 			{
@@ -967,7 +967,7 @@ public class RoomScript : MonoBehaviour
 		else
 		{
 			Debug.Log("Не хватает ресов для починки!");
-			EventManager.callWarning.Invoke($"Не хватает <color=yellow>{10 - await GameManager.Instance.GetAsteriy()}</color> астерия для починки!");
+			EventManager.callWarning.Invoke($"Не хватает <color=yellow>{ValuesHolder.RepairCost - await GameManager.Instance.GetAsteriy()}</color> астерия для починки!");
 			return;
 		}
 
