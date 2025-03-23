af4c363ac01a964bd799b9bfeda353970513ba5b
Rostozzza <gfiyskgs@gmail.com>
2025-03-23 14:58:27 +0300

    Метеориты трясут экран
    ааааааааа без тряски

--- Assets/Scripts/Game/GameManager.cs
+++ Assets/Scripts/Game/GameManager.cs
@@ -1850,7 +1850,7 @@ public class GameManager : MonoBehaviour
 					Debug.Log(room.name + " задамажен фазой на " + damage);
 					room.GetComponent<RoomScript>().ChangeDurability(-damage);
 				});
-				if (!ShopManager.Instance.GetComponent<Animator>().GetCurrentAnimatorStateInfo(0).IsName("TabletHide")) Camera.main.GetComponent<CameraShake>().MeteorImpact();
+				if (!ShopManager.Instance.GetComponent<Animator>().GetCurrentAnimatorStateInfo(0).IsName("TabletShow")) Camera.main.GetComponent<CameraShake>().MeteorImpact();
 				yield return new WaitForSeconds(ValuesHolder.CycleDuration / (4 * meteorAmount));
 			}
 		}
